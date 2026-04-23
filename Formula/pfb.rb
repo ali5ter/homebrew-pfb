43d55560ebce8a7476c0070c39cb4654dfeee4c5
Alister Lewis-Bowen <alister@lewis-bowen.org>
2026-04-23 17:12:08 -0400

    chore: bump pfb formula to v2.5.0

--- Formula/pfb.rb
+++ Formula/pfb.rb
@@ -1,8 +1,8 @@
 class Pfb < Formula
   desc "Pretty feedback for Bash scripts — lightweight terminal UI components"
   homepage "https://github.com/ali5ter/pfb"
-  url "https://github.com/ali5ter/pfb/archive/refs/tags/v2.4.0.tar.gz"
-  sha256 "3a57652d8c856a0cad3e72e003723214c1233ae8626e3c03e34b21b24081807a"
+  url "https://github.com/ali5ter/pfb/archive/refs/tags/v2.5.0.tar.gz"
+  sha256 "be3ec6a6677b78bd51ecb2ed412678cdfaac2463e782dd03ca4057ae27d373db"
   license "MIT"
 
   def install
